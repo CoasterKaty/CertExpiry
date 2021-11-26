6ddb4b0309ae3d7f9dc798e8e05c85fc9cbbc9c6
katy <katy@katynicholson.uk>
2021-11-26 11:45:52 +0000

    Fix issue 2

--- inc/alert_mail_task.php
+++ inc/alert_mail_task.php
@@ -26,33 +26,38 @@ class alertMailTask  extends modCertTools {
 					$apps = $this->getApps(0, $pageCount, $statuses);
 					// Output table showing Warning first, then Expired. Only show items which haven't been muted.
 					$alertMuted = 0;
+					$alertCount = 0;
 					$alertTable = '<table><thead><tr><td>Status</td><td>Expiry Date</td><td>Item</td><td>Type</td></tr></thead><tbody>';
 					foreach ($apps as $app) {
 						if ($app['status'] == 'WARN' && $app['muteAlert'] != '1') {
 							$alertTable .= '<tr><td>Warning</td><td>' . date('Y-m-d', strtotime($app['endDateTime'])) . '</td><td>' . ($app['link'] ? ' <a href="' . $app['link'] . '">' . $app['displayName'] . '</a>' : $app['displayName']) . '</td><td>' . $app['typeDisplayName'] . '</td></tr>';
+							$alertCount++;
 						}
 						if ($app['status'] == 'WARN' && $app['muteAlert']) $alertMuted++;
 					}
 					foreach ($apps as $app) {
 						if ($app['status'] == 'EXPIRED' && $app['muteAlert'] != '1') {
 							$alertTable .= '<tr><td>Expired</td><td>' . date('Y-m-d', strtotime($app['endDateTime'])) . '</td><td>' . ($app['link'] ? ' <a href="' . $app['link'] . '">' . $app['displayName'] . '</a>' : $app['displayName']) . '</td><td>' . $app['typeDisplayName'] . '</td></tr>';
+							$alertCount++;
 						}
 						if ($app['status'] == 'EXPIRED' && $app['muteAlert']) $alertMuted++;
 					}
 
 					$alertTable .= '</tbody></table>';
-					$alertMessage = $this->settings['alertTemplate'];
-					$alertMessage = str_replace('{$alertMuted}', $alertMuted, $alertMessage);
-					$alertMessage = str_replace('{$alertDays}', $this->settings['alertDays'], $alertMessage);
-					$alertMessage = str_replace('{$data}', $alertTable, $alertMessage);
-					$alertMessage = str_replace('{$url}', '<a href="' . _URL . '">' . _URL . '</a>', $alertMessage);
+					if ($alertCount) {
+						$alertMessage = $this->settings['alertTemplate'];
+						$alertMessage = str_replace('{$alertMuted}', $alertMuted, $alertMessage);
+						$alertMessage = str_replace('{$alertDays}', $this->settings['alertDays'], $alertMessage);
+						$alertMessage = str_replace('{$data}', $alertTable, $alertMessage);
+						$alertMessage = str_replace('{$url}', '<a href="' . _URL . '">' . _URL . '</a>', $alertMessage);
 
-					foreach ($recipients as $recipient) {
-						$mailArgs = array(	'subject' => 'Expiring Certificates Notification',
-									'importance' => 'High',
-									'toRecipients' => array(array('address' => $recipient['txtEmail'])),
-									'body' => $alertMessage);
-						$this->modGraph->sendMail($this->settings['senderEmail'], $mailArgs);
+						foreach ($recipients as $recipient) {
+							$mailArgs = array(	'subject' => 'Expiring Certificates Notification',
+										'importance' => 'High',
+										'toRecipients' => array(array('address' => $recipient['txtEmail'])),
+										'body' => $alertMessage);
+							$this->modGraph->sendMail($this->settings['senderEmail'], $mailArgs);
+						}
 					}
 					$this->saveSettings(array('alertLastRun' => date('Y-m-d')));
 				}
